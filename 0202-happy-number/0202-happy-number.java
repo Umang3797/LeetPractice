46fba187c5e05ef910b599ba26d5172f75cd968c
Umang3797 <35635165+Umang3797@users.noreply.github.com>
2024-01-01 18:21:51 -0700

    Time: 4 ms (6.72%), Space: 40.7 MB (15.98%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet();
+        while (true) {
+            n = numSquareSum(n);
+            if (n == 1)
+                return true;
+            if (set.contains(n))
+                return false;
+            set.add(n);
+        }
+    }
+    private int numSquareSum(int n){
+        int squareSum = 0;
+        while (n!= 0){
+            squareSum += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        return squareSum;
+    }
+}
