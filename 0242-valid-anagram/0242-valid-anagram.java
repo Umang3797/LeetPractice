4871c716c6995c37d4eed7fc23f78e5e04a73e5b
Umang3797 <35635165+Umang3797@users.noreply.github.com>
2024-01-02 17:04:53 -0700

    Time: 6 ms (37.27%), Space: 42.5 MB (76.22%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char a[] = new char[26];
+        
+        for(int i=0;i<s.length();i++){
+            a[s.charAt(i)-'a']++;
+        }
+        
+        for(int i=0;i<t.length();i++){
+            a[t.charAt(i)-'a']--;
+        }
+        
+        for(int i=0;i<26;i++){
+            if(a[i]>0)
+                return false;
+        }
+        return true;
+        
+        
+        
+    }
+}
