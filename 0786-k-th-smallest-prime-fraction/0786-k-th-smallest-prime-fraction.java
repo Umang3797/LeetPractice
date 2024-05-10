6e0e1080e18d87d88a663bb9c2fea2b7ba59b262
Umang3797 <35635165+Umang3797@users.noreply.github.com>
2024-05-09 19:52:39 -0700

    Time: 409 ms (46.29%), Space: 133.9 MB (5.24%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.java
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.java
@@ -1,25 +1,27 @@
 class Solution {
     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
-        PriorityQueue<Float> minheap = new PriorityQueue(Comparator.reverseOrder());
+        PriorityQueue<Float[]> maxheap = new PriorityQueue<>((a,b)->Float.compare(b[0], a[0]));
         
-        Map<Float,String> map = new HashMap();
+        // Map<Float,String> map = new HashMap();
         
         for(int i=0;i<arr.length;i++){
             for(int j=i+1;j<arr.length;j++){
-                minheap.add((float)arr[i] / (float)arr[j]);
-                map.put((float)arr[i] / (float)arr[j],arr[i]+" "+arr[j]);
-                if(minheap.size()>k){
-                    minheap.remove();
+                maxheap.add(new Float[]{(float)arr[i] / (float)arr[j], (float)arr[i], (float)arr[j]});
+                // map.put((float)arr[i] / (float)arr[j],arr[i]+" "+arr[j]);
+                if(maxheap.size()>k){
+                    maxheap.remove();
                 }
             }
         }
-        String ans=map.get(minheap.remove());
-        System.out.println(ans);
-        String[] parts = ans.split(" ");
-        int[] result = new int[parts.length];
-        for (int i = 0; i < parts.length; i++) {
-            result[i] = Integer.parseInt(parts[i]);
-        }
-        return result;
+        // String ans=map.get(maxheap.remove());
+        // String[] parts = ans.split(" ");
+        // int[] result = new int[parts.length];
+        // for (int i = 0; i < parts.length; i++) {
+        //     result[i] = Integer.parseInt(parts[i]);
+        // }
+        
+        Float[]ans = maxheap.remove();
+        return new int[]{Math.round(ans[1]), Math.round(ans[2])};
+
     }
 }
