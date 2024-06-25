c59116c89a79022837c4c170d924fe977c8b9885
Umang3797 <35635165+Umang3797@users.noreply.github.com>
2024-06-25 02:01:34 -0700

    Time: 0 ms (100.00%) | Memory: 40.2 MB (63.70%) - LeetSync

---
+++ 62-unique-paths/unique-paths.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int dp[][] = new int[m][n];
+        for (int[] row : dp)
+            Arrays.fill(row, -1);
+        
+        return f(m-1,n-1,dp);
+    }
+    private static int f(int m, int n,int[][] dp){
+        if(m<0 || n<0) return 0;
+        if(m==0 && n==0) return 1;
+        if(dp[m][n]!=-1) return dp[m][n];
+        
+        int left = f(m,n-1,dp);
+        int up = f(m-1,n,dp);
+        
+        dp[m][n] = left+up;
+        return dp[m][n];
+    }
+}
