7427b1a740fe407031b51e2188e08ddf537e2fc5
Umang Mavani <35635165+Umang3797@users.noreply.github.com>
2024-09-24 20:32:22 -0700

    Time: 1964 ms (45.86%) | Memory: 306.3 MB (70.34%) - LeetSync

--- 2494-sum-of-prefix-scores-of-strings/sum-of-prefix-scores-of-strings.py
+++ 2494-sum-of-prefix-scores-of-strings/sum-of-prefix-scores-of-strings.py
@@ -1,37 +1,40 @@
 class TrieNode:
     def __init__(self):
-        self.children={}
-        self.count=0
-        
-class Trie:
-    def __init__(self):
-        self.trie = TrieNode()
-        
-    def insert(self,word):
-        node=self.trie
+        self.children = {}
+        self.count = 0
+
+    # Inserting a word into the Trie
+    def insert(self, word):
+        node = self  # Start from the root node (current instance)
         
         for char in word:
             if char not in node.children:
-                node.children[char]=TrieNode()
+                node.children[char] = TrieNode()
             
-            node=node.children[char]
-            node.count+=1
+            node = node.children[char]
+            node.count += 1  # Increase the count at each node
     
-    def count(self,word):
-        node=self.trie
-        count=0
+    # Counting the prefix score for a given word
+    def count_prefix(self, word):
+        node = self  # Start from the root node (current instance)
+        count = 0
         
         for char in word:
-            count+=node.children[char].count
-            node=node.children[char]
+            if char in node.children:
+                count += node.children[char].count
+                node = node.children[char]
+            else:
+                break  # If a character in the word is not found in the Trie, stop
         
         return count
 
+
 class Solution:
     def sumPrefixScores(self, words: List[str]) -> List[int]:
-        trie = Trie()
+        trie = TrieNode()
+        # Insert all words into the Trie
         for word in words:
             trie.insert(word)
-            
-        return [trie.count(word) for word in words]
         
+        # Return the prefix scores for each word
+        return [trie.count_prefix(word) for word in words]
