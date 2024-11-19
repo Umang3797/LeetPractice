4a5aa57ea0d92e0acffb6ed0704e95e2bf22e7dc
Umang Mavani <35635165+Umang3797@users.noreply.github.com>
2024-11-18 18:55:28 -0700

    Time: 8 ms (15.81%) | Memory: 52.9 MB (57.90%) - LeetSync

---
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
+        int[][] ans = new int[rowSum.length][colSum.length];
+        
+        for(int[] arr: ans)
+            Arrays.fill(arr, 0);
+        
+        for(int i=0;i<rowSum.length;i++){
+            for(int j=0;j<colSum.length;j++){
+                ans[i][j] = Math.min(rowSum[i] , colSum[j]);
+                rowSum[i]-= ans[i][j];
+                colSum[j]-= ans[i][j];
+            }
+        }
+        
+        
+        
+        return ans;
+    }
+}
