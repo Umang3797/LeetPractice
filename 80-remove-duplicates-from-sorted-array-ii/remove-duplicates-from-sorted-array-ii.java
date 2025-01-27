36be17280de1d8cae34415cabfc80b9ef6ab9e9a
Umang Mavani <35635165+Umang3797@users.noreply.github.com>
2025-01-27 12:51:07 -0700

    Time: 0 ms (100.00%) | Memory: 44.9 MB (17.74%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -1,16 +1,23 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        if (nums.length <= 2) {
-            return nums.length;
-        }
-        
-        int ans = 2;  // Start with 2 because the first two elements are always valid
-        for (int i = 2; i < nums.length; i++) {
-            if (nums[i] != nums[ans - 2]) {  
-                nums[ans] = nums[i];// Place the current element at next valid position
-                ans++;
+        int j=1;
+        int count=1;
+        int n=nums.length;
+
+        for(int i=1;i<n;i++){
+            if(nums[i]==nums[i-1]){
+                count++;
+            }else{
+                count=1;
+            }
+
+            if(count<=2){
+                nums[j]=nums[i];
+                j++;
             }
         }
-        return ans;
+
+        return j;
+
     }
 }
