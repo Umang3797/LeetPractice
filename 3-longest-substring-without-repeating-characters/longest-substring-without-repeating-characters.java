d10967a3b4cff70b0e49981a853bfe43a97cdb43
Umang Mavani <35635165+Umang3797@users.noreply.github.com>
2025-04-19 20:41:01 -0700

    Time: 6 ms (70.55%) | Memory: 44.9 MB (42.89%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,15 +1,46 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if (s.length()==0) return 0;
-        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-        int max=0;
-        for (int i=0, j=0; i<s.length(); ++i){
-            if (map.containsKey(s.charAt(i))){
-                j = Math.max(j,map.get(s.charAt(i))+1);
+        Set<Character> set = new HashSet();
+        int left=0;
+        int maxLength=0;
+        
+        for(int right=0;right<s.length();right++){
+            while(set.contains(s.charAt(right))){
+                set.remove(s.charAt(left));
+                left++;
             }
-            map.put(s.charAt(i),i);
-            max = Math.max(max,i-j+1);
+            set.add(s.charAt(right));
+            maxLength=Math.max(maxLength,right-left+1);
         }
-        return max;
+        return maxLength;
+        
+        
+        
+        
+        
+        
+        
+        
+        // Map<Character, Integer> map = new HashMap<>();
+        // int i = 0;
+        // int j = 0;
+        // int max = 0;
+        // while(j < s.length()){
+        //     map.put(s.charAt(j), map.getOrDefault(s.charAt(j), 0) + 1);
+        //     if(map.size() == j - i + 1){
+        //         max = Math.max(max, j - i + 1);
+        //         j++;
+        //     }
+        //     else if(map.size() < j - i + 1){
+        //         while(map.size() < j - i + 1){
+        //             map.put(s.charAt(i), map.get(s.charAt(i)) - 1);
+        //             if(map.get(s.charAt(i)) == 0) 
+        //                 map.remove(s.charAt(i));
+        //             i++;
+        //         }
+        //         j++;
+        //     }
+        // }
+        // return max;
     }
 }
