524f58278658be1f9ae76f7657077286fb1a17bc
Umang Mavani <35635165+Umang3797@users.noreply.github.com>
2025-05-06 19:15:03 -0700

    Time: 8 ms (86.75%) | Memory: 46.3 MB (92.09%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -2,13 +2,14 @@ class Solution {
     public int[][] merge(int[][] intervals) {
         if(intervals.length<=1)
             return intervals;
-        Arrays.sort(intervals, (a, b) -> {
-            if (a[0] != b[0]) {
-                return Integer.compare(a[0], b[0]);
-            } else {
-                return Integer.compare(a[1], b[1]);
-            }
-        });
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        // Arrays.sort(intervals, (a, b) -> {
+        //     if (a[0] != b[0]) {
+        //         return Integer.compare(a[0], b[0]);
+        //     } else {
+        //         return Integer.compare(a[1], b[1]);
+        //     }
+        // });
         List<int[]> list = new ArrayList();
         int[] current_interval= intervals[0];
         list.add(current_interval);
