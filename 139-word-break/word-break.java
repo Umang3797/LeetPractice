8fd92d64b9df19b4c0a4716b6e69bbcc8e0f5154
Umang Mavani <35635165+Umang3797@users.noreply.github.com>
2025-05-21 17:01:18 -0700

    Time: 8 ms (42.11%) | Memory: 44 MB (58.78%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -3,16 +3,18 @@ class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         if (wordDict.contains(s))
                 return true;
-            if (map.containsKey(s))
-                return map.get(s);
-            for (int i = 1; i <= s.length(); i++) {
-                String left = s.substring(0, i);
-                if (wordDict.contains(left) && wordBreak(s.substring(i), wordDict)) {
-                    map.put(s, true);
-                    return true;
-                }
+        
+        if (map.containsKey(s))
+            return map.get(s);
+        
+        for (int i = 0; i <= s.length(); i++) {
+            String left = s.substring(0, i);
+            if (wordDict.contains(left) && wordBreak(s.substring(i), wordDict)) {
+                map.put(s, true);
+                return true;
             }
-            map.put(s, false);
-            return false;
+        }
+        map.put(s, false);
+        return false;
     }
 }
