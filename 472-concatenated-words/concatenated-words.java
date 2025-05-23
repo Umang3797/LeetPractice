bf41af04b59fdc01cf34dad13b08ba0a6902a5e0
Umang Mavani <35635165+Umang3797@users.noreply.github.com>
2025-05-23 11:56:09 -0700

    Time: 66 ms (50.76%) | Memory: 53.2 MB (30.44%) - LeetSync

---
+++ 472-concatenated-words/concatenated-words.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<String> findAllConcatenatedWordsInADict(String[] words) {
+        if(words==null || words.length==0)
+            return new ArrayList();
+        
+        Set<String> set = new HashSet();
+        List<String> ans = new ArrayList();        
+        Map<String, Boolean> memo = new HashMap<>();
+
+        
+        for(String s : words)
+            set.add(s);
+        
+        for(String s : words) {
+            if(isConcat(s, set, memo)) {
+                ans.add(s);
+            }
+        }
+        return ans;
+    }
+    private boolean isConcat(String s, Set<String> set,Map<String, Boolean> memo){
+        if (memo.containsKey(s)) 
+            return memo.get(s);
+        
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            String suffix = s.substring(i);
+            if(set.contains(s.substring(0,i)) && (set.contains(suffix) || isConcat(suffix,set,memo))){
+                memo.put(s, true);
+                return true;
+            }
+            
+        }
+        memo.put(s, false);
+        return false;
+    }
+}
