ea75fb1dfe0ddf7066a576c122cc3e98df4e14c8
Umang Mavani <35635165+Umang3797@users.noreply.github.com>
2025-07-04 19:11:07 -0700

    Time: 0 ms (100.00%) | Memory: 51.4 MB (36.13%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.java
+++ 975-range-sum-of-bst/range-sum-of-bst.java
@@ -18,7 +18,7 @@ class Solution {
         if (root == null) return 0; // base case.
         if (root.val < L) return rangeSumBST(root.right, L, R); // left branch excluded.
         if (root.val > R) return rangeSumBST(root.left, L, R); // right branch excluded.
-        return root.val + rangeSumBST(root.right, L, R) + rangeSumBST(root.left, L, R); // count in both children.
+        return root.val + rangeSumBST(root.left, L, R) + rangeSumBST(root.right, L, R); // count in both children.
         
     }
 }
