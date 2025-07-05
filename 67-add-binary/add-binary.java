b6bb9356c2ccbf5ed01992146a4e7581dfd97fda
Umang Mavani <35635165+Umang3797@users.noreply.github.com>
2025-07-05 11:37:02 -0700

    Time: 1 ms (99.79%) | Memory: 42.2 MB (66.05%) - LeetSync

---
+++ 67-add-binary/add-binary.java
@@ -0,0 +1,34 @@
+// class Solution {
+//     public String addBinary(String a, String b) {
+//         return Integer.toBinaryString(
+//             Integer.parseInt(a, 2) + Integer.parseInt(b, 2)
+//         );
+//     }
+// }
+class Solution {
+    public String addBinary(String a, String b) {
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+        
+        StringBuilder sb = new StringBuilder();
+        
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int sum = carry;
+            
+            if (i >= 0) {
+                sum += a.charAt(i) - '0';
+                i--;
+            }
+            if (j >= 0) {
+                sum += b.charAt(j) - '0';
+                j--;
+            }
+            
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+        
+        return sb.reverse().toString();
+    }
+}
