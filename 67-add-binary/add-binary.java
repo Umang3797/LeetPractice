0b3a3f19ccc9ddd016eac46241b3554b968160b9
Umang Mavani <35635165+Umang3797@users.noreply.github.com>
2025-07-05 11:45:40 -0700

    Time: 1 ms (99.79%) | Memory: 42.3 MB (66.05%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -7,28 +7,33 @@
 // }
 class Solution {
     public String addBinary(String a, String b) {
-        int i = a.length() - 1;
-        int j = b.length() - 1;
+        
+        StringBuilder res = new StringBuilder();
         int carry = 0;
+        int p1 = a.length() - 1;
+        int p2 = b.length() - 1;
         
-        StringBuilder sb = new StringBuilder();
-        
-        while (i >= 0 || j >= 0 || carry != 0) {
-            int sum = carry;
+        while (p1 >= 0 || p2 >= 0 || carry != 0) {
+            int x1 = (p1 >= 0) ? a.charAt(p1) - '0' : 0;
+            int x2 = (p2 >= 0) ? b.charAt(p2) - '0' : 0;
             
-            if (i >= 0) {
-                sum += a.charAt(i) - '0';
-                i--;
-            }
-            if (j >= 0) {
-                sum += b.charAt(j) - '0';
-                j--;
-            }
-            
-            sb.append(sum % 2);
+            int sum = x1 + x2 + carry;
+            res.append(sum % 2);
             carry = sum / 2;
+            
+            p1--;
+            p2--;
         }
         
-        return sb.reverse().toString();
+        if (carry != 0)
+            res.append(carry);
+        
+        return res.reverse().toString();
     }
 }
+
+
+
+       
+        
+        
