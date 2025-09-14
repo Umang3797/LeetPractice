778831306298baf590e9f30a85b47b968c5c200a
Umang Mavani <35635165+Umang3797@users.noreply.github.com>
2025-09-14 13:51:00 -0700

    Time: 15 ms (37.70%) | Memory: 49 MB (25.33%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -8,16 +8,32 @@ class Solution {
             count.put(n, count.getOrDefault(n, 0) + 1);
         }
         // Queue<Integer> maxHeap = new PriorityQueue<>((n1, n2) -> count.get(n2) - count.get(n1));
-        Queue<Integer> maxHeap = new PriorityQueue<>((n1, n2) -> 
-        Integer.compare(count.get(n2), count.get(n1)));
+        // Queue<Integer> maxHeap = new PriorityQueue<>((n1, n2) -> 
+        // Integer.compare(count.get(n2), count.get(n1)));
 
+        //min heap
+        Queue<Integer> minHeap = new PriorityQueue<>((n1, n2) -> 
+        Integer.compare(count.get(n1), count.get(n2)));
+        
         for (int n : count.keySet()) {
-            maxHeap.add(n);
+            minHeap.add(n);
+            if(minHeap.size()>k){
+                minHeap.poll();
+            }
+            
         }
         int[] top = new int[k];
         for (int i = 0; i < k; ++i) {
-            top[i] = maxHeap.poll();
+            top[i] = minHeap.poll();
         }
+
+        // for (int n : count.keySet()) {
+        //     maxHeap.add(n);
+        // }
+        // int[] top = new int[k];
+        // for (int i = 0; i < k; ++i) {
+        //     top[i] = maxHeap.poll();
+        // }
         return top;
     }
 }
