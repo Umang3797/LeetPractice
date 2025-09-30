8f184bdb7470dca5fc61081ededb1b523095c0ee
Umang Mavani <35635165+Umang3797@users.noreply.github.com>
2025-09-30 16:29:06 -0700

    Time: 301 ms (8.31%) | Memory: 44.8 MB (46.18%) - LeetSync

--- 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.java
+++ 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.java
@@ -7,14 +7,22 @@ class Solution {
         }
 
         if(nums.length==1) return nums[0];
-        while(list.size()>1){
-            int i=0;
-            List<Integer> newList= new ArrayList(list.size()-1);
-            while(i<list.size()-1){
-                newList.add((list.get(i)+list.get(i+1))%10);
-                i++;
+        // while(list.size()>1){
+        //     int i=0;
+        //     List<Integer> newList= new ArrayList(list.size()-1);
+        //     while(i<list.size()-1){
+        //         newList.add((list.get(i)+list.get(i+1))%10);
+        //         i++;
+        //     }
+        //     list=newList;
+        // }
+
+        while (list.size() > 1) {
+            List<Integer> next = new ArrayList<>(list.size() - 1);
+            for (int i = 0; i < list.size() - 1; i++) {
+                next.add((list.get(i) + list.get(i + 1)) % 10);
             }
-            list=newList;
+            list = next;
         }
     
         
